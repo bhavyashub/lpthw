3cbc222422fdd96897306defeebe350e81d4cea7
Bhavya Devarakonda <bhavya.devarakonda@gmail.com>
2017-06-10 19:11:56 -0700

    Adding new python exercisess

---
+++ ex1.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("Yay! Printing")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print("I could have code like this.") #and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print ("this will run.")
