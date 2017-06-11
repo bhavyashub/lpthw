67ed02ffed8af45c8d73f1d8be4bfc34af8d8f91
Naveen Purushotham <naveenpurushotham@Bhavyas-MacBook-Pro.local>
2017-06-10 19:59:53 -0700

    updating with multi print loop

--- ex1.py
+++ ex1.py
@@ -10,4 +10,5 @@ print("I could have code like this.") #and the comment after is ignored
 # You can also use a comment to "disable" or comment out code:
 # print("This won't run.")
 
-print ("this will run.")
+for i in range(10):
+    print(("this will run.")
