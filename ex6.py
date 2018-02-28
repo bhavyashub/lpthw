04baad370e53449660961cc444f29ab3fde698e5
Bhavya Devarakonda <bhavya.devarakonda@gmail.com>
2018-02-28 10:43:32 -0800

    Adding ex6 to master

---
+++ ex6.py
@@ -0,0 +1,25 @@
+types_of_people = 10 #Set variable for types of people.
+x = f"There are {types_of_people} types of people." #Set variable x for a statement string
+
+binary  = "binary" #string variable
+do_not = "don't"  #string variable
+y = f"Those who know {binary} and those who {do_not}."  #set variable y for a statement string
+
+print(x)  #prints x
+print(y)  #prints y
+
+print(f"I said: {x}")  #prints formatted x
+print(f"I also said: {y}")  #prints formatted y87g
+
+hilarious = False #set variable to false.
+
+joke_evaluation = "Isn't that joke so funny?! {}"  #set variable with empty{}, which will be used in a later instance.
+
+print(joke_evaluation.format(hilarious))  #prints string with the pre-defined format value.
+
+w = "This is the left side of..." #string variable w
+e = "a string with a right side"  #string variable e
+
+print(w + e)  #print w and e - this concatenates string w and string e
+
+
